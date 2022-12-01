e1623899a65055bbb9dbf828fbc0a833d6444a3c
Sebastian Letelier <sebalete10@gmail.com>
2022-12-01 01:14:03 -0300

    Terminado

---
+++ db/databse.sql
@@ -0,0 +1,20 @@
+--FUE CREADO EN DOCKER --> docker run -e MYSQL_ROOT_PASSWORD=password --name mymysql mysql
+-- CREATE DATABASE IF NOT EXISTS db_name;
+
+-- USE db_name;
+
+-- CREATE TABLE employee(
+--     id INT(11) NOT NULL AUTO_INCREMENT,
+--     name VARCHAR(45) DEFAULT NULL,
+--     salary INT(5) DEFAULT NULL,
+--     PRIMARY KEY (id)
+-- );
+
+
+-- INSERT INTO employee VALUES
+--     (1,'JOE', 1000),
+--     (2,'Henry', 2000),
+--     (3,'Sam', 3000),
+--     (4,'Max', 4000);
+
+    
